87d99bdaebe8facdd1826f6b8def42b7f3c7ee12
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-08 22:54:09 -0400

    instial commit

---
+++ ASWTLJSVariablesforStrings.js
@@ -0,0 +1,63 @@
+// In the following code, "Mark" is a string. What is name (answer with 1 word)
+ varible
+var name = "Mark"; 
+//In one word, if it is enclosed in quotation marks, what is it?
+string
+//What is the keyword that declares a variable?
+var name;
+
+/*This statement...
+userName = "buddy5000";
+...is correct only if the variable has already been _______.       */                
+declared
+//Form a statement that declares the variable punchline and assigns a value to it.
+var punchline = "The Accountant";
+/*In a single statement, 
+declare the variable nickname and assign the string "Satchmo" to it.*/
+var name = "Satchmo";
+//Declare the variable teamName.
+var teamName;
+/*In a single statement declare a variable and assign 
+a string of your choice to it. (You've learned only one rule for naming 
+variables so far, so it's the only one I'll hold you to.)*/
+var name = "Justin";
+/*Declare a variable. (You've learned only one rule for naming variables so far,
+so it's the only one I'll hold you to.)*/
+var year;
+// This statement has already been coded.
+// var bestMan = "Charlie";
+// Assign the variable a new string.
+bestMan = "Emanuel";
+// Declare a variable without defining it. Then, in a second statement, assign it a string. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+var name;
+name = "Justin";
+// In a single statement declare a variable of your choice and assign a string of your choice to it.
+var team = "Detroit Pistons";
+// Declare a variable of your choice.
+var year ;
+// Assign a string of your choice to a variable of your choice that has already been declared.
+var dog = "Snuggles";
+// This statement has already been coded.
+// var bestMan = "Charlie";
+bestMan = "Chachovar";
+// Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+var dog;
+dog = "Snuggles";
+// In the first statement declare a variable of your choice and assign it a string of your choice.
+var favFood = "Pizza";
+favFood = "Litlte ceasears";
+// Declare a variable of your choice and assign it a string of your choice. Then code an alert, specifying the variable, not the string, as the message.
+var dog = "Snuggles";
+alert(dog);
+// Declare a variable of your choice and assign it a string that's exactly the same as the variable name, but enclosed in quotation marks so JavaScript recognizes it as a string. Then code an alert, specifying the variable, not the string, as the message. 
+var animal = "animal";
+alert(animal);
+
+
+
+
+
+                            
+
+
+                            
